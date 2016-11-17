dedc5d709849170be38e6ab8ef9968db2299df01
Sebastian Krajewski <mail@wieku.me>
2016-11-17 17:57:55 +0100

    Added link to coypright's label
    Signed-off-by: Sebastian Krajewski <mail@wieku.me>

--- core/src/xyz/hexagons/client/menu/screens/MainMenu.java
+++ core/src/xyz/hexagons/client/menu/screens/MainMenu.java
@@ -147,7 +147,13 @@ public class MainMenu implements Screen {
 		version.setPosition(5, stage.getHeight() - version.getHeight() - 5);
 		stage.addActor(version);
 
-		copyright = new Label("Hexagons! 2016 Written by Wieku (wieku.me)", GUIHelper.getLabelStyle(new Color(0xa0a0a0ff), 10));
+		copyright = new Label("Hexagons! 2016 Written by Wieku and Magik6k", GUIHelper.getLabelStyle(new Color(0xa0a0a0ff), 10));
+		copyright.addListener(new InputListener(){
+			public boolean touchDown(InputEvent event, float x, float y,int pointer, int button) {
+				Gdx.net.openURI("https://hexagons.xyz/");
+				return true;
+			}
+		});
 		copyright.pack();
 		copyright.setPosition(stage.getWidth() - copyright.getWidth() - 5, 5);
 		stage.addActor(copyright);
