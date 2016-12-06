35f66a5820cbd270025b0123609d034aa797d282
Sebastian Krajewski <mail@wieku.me>
2016-12-06 21:25:08 +0100

    New OpenAl Player, but some bugs can still occur due to ogg in zip compression

---
+++ desktop/src/com/badlogic/gdx/backends/lwjgl/audio/OpenALPlayer.java
@@ -0,0 +1,306 @@
+package com.badlogic.gdx.backends.lwjgl.audio;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import com.badlogic.gdx.utils.StreamUtils;
+import org.lwjgl.BufferUtils;
+import static org.lwjgl.openal.AL10.AL_BUFFERS_PROCESSED;
+import static org.lwjgl.openal.AL10.AL_BUFFERS_QUEUED;
+import static org.lwjgl.openal.AL10.AL_FALSE;
+import static org.lwjgl.openal.AL10.AL_FORMAT_MONO16;
+import static org.lwjgl.openal.AL10.AL_FORMAT_STEREO16;
+import static org.lwjgl.openal.AL10.AL_GAIN;
+import static org.lwjgl.openal.AL10.AL_INVALID_VALUE;
+import static org.lwjgl.openal.AL10.AL_LOOPING;
+import static org.lwjgl.openal.AL10.AL_NO_ERROR;
+import static org.lwjgl.openal.AL10.AL_PLAYING;
+import static org.lwjgl.openal.AL10.AL_POSITION;
+import static org.lwjgl.openal.AL10.AL_SOURCE_STATE;
+import static org.lwjgl.openal.AL10.alBufferData;
+import static org.lwjgl.openal.AL10.alDeleteBuffers;
+import static org.lwjgl.openal.AL10.alGenBuffers;
+import static org.lwjgl.openal.AL10.alGetError;
+import static org.lwjgl.openal.AL10.alGetSourcef;
+import static org.lwjgl.openal.AL10.alGetSourcei;
+import static org.lwjgl.openal.AL10.alSource3f;
+import static org.lwjgl.openal.AL10.alSourcePause;
+import static org.lwjgl.openal.AL10.alSourcePlay;
+import static org.lwjgl.openal.AL10.alSourceQueueBuffers;
+import static org.lwjgl.openal.AL10.alSourceStop;
+import static org.lwjgl.openal.AL10.alSourceUnqueueBuffers;
+import static org.lwjgl.openal.AL10.alSourcef;
+import static org.lwjgl.openal.AL10.alSourcei;
+import org.lwjgl.openal.AL11;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+/** @author Nathan Sweet with Sebastian Krajewski modifications */
+public class OpenALPlayer extends OpenALMusic {
+	static private final int bufferSize = 8192;
+	static private final int bufferCount = 3;
+	static private final int bytesPerSample = 2;
+	static public final byte[] tempBytes = new byte[bufferSize];
+	static private final ByteBuffer tempBuffer = BufferUtils.createByteBuffer(bufferSize);
+	
+	private final OpenALAudio audio;
+	private IntBuffer buffers;
+	private int sourceID = -1;
+	private int format, sampleRate;
+	private boolean isLooping, isPlaying;
+	private float volume = 1;
+	private float pan = 0;
+	private float renderedSeconds, secondsPerBuffer;
+	
+	
+	private OggInputStream input;
+	private OggInputStream previousInput;
+	protected FileHandle file = null;
+	protected int bufferOverhead = 0;
+	
+	private OnCompletionListener onCompletionListener;
+	
+	public OpenALPlayer () {
+		super(null, null);
+		this.audio = (OpenALAudio) Gdx.audio;
+		this.onCompletionListener = null;
+	}
+	
+	
+	public void setFile(FileHandle file) {
+		if (audio.noDevice) return;
+		this.file = file;
+		input = new OggInputStream(file.read());
+		setup(input.getChannels(), input.getSampleRate());
+	}
+	
+	
+	protected void setup (int channels, int sampleRate) {
+		this.format = channels > 1 ? AL_FORMAT_STEREO16 : AL_FORMAT_MONO16;
+		this.sampleRate = sampleRate;
+		secondsPerBuffer = (float)(bufferSize - bufferOverhead)  / (bytesPerSample * channels * sampleRate);
+	}
+	
+	public void play () {
+		if (audio.noDevice) return;
+		if (sourceID == -1) {
+			sourceID = audio.obtainSource(true);
+			if (sourceID == -1) return;
+			
+			audio.music.add(this);
+			
+			if (buffers == null) {
+				buffers = BufferUtils.createIntBuffer(bufferCount);
+				alGenBuffers(buffers);
+				if (alGetError() != AL_NO_ERROR) throw new GdxRuntimeException("Unable to allocate audio buffers.");
+			}
+			alSourcei(sourceID, AL_LOOPING, AL_FALSE);
+			setPan(pan, volume);
+			
+			boolean filled = false; // Check if there's anything to actually play.
+			for (int i = 0; i < bufferCount; i++) {
+				int bufferID = buffers.get(i);
+				if (!fill(bufferID)) break;
+				filled = true;
+				alSourceQueueBuffers(sourceID, bufferID);
+			}
+			if (!filled && onCompletionListener != null) onCompletionListener.onCompletion(this);
+			
+			if (alGetError() != AL_NO_ERROR) {
+				stop();
+				return;
+			}
+		}
+		if (!isPlaying) {
+			alSourcePlay(sourceID);
+			isPlaying = true;
+		}
+	}
+	
+	public void stop () {
+		if (audio.noDevice) return;
+		if (sourceID == -1) return;
+		//audio.music.removeValue(this, true);
+		reset();
+		//audio.freeSource(sourceID);
+		//sourceID = -1;
+		renderedSeconds = 0;
+		isPlaying = false;
+	}
+	
+	public void pause () {
+		if (audio.noDevice) return;
+		if (sourceID != -1) alSourcePause(sourceID);
+		isPlaying = false;
+	}
+	
+	public boolean isPlaying () {
+		if (audio.noDevice) return false;
+		if (sourceID == -1) return false;
+		return isPlaying;
+	}
+	
+	public void setLooping (boolean isLooping) {
+		this.isLooping = isLooping;
+	}
+	
+	public boolean isLooping () {
+		return isLooping;
+	}
+	
+	public void setVolume (float volume) {
+		this.volume = volume;
+		if (audio.noDevice) return;
+		if (sourceID != -1) alSourcef(sourceID, AL_GAIN, volume);
+	}
+	
+	public float getVolume () {
+		return this.volume;
+	}
+	
+	public void setPan (float pan, float volume) {
+		this.volume = volume;
+		this.pan = pan;
+		if (audio.noDevice) return;
+		if (sourceID == -1) return;
+		alSource3f(sourceID, AL_POSITION, MathUtils.cos((pan - 1) * MathUtils.PI / 2), 0,
+				MathUtils.sin((pan + 1) * MathUtils.PI / 2));
+		alSourcef(sourceID, AL_GAIN, volume);
+	}
+	
+	public void setPosition (float position) {
+		if (audio.noDevice) return;
+		if (sourceID == -1) return;
+		boolean wasPlaying = isPlaying;
+		isPlaying = false;
+		alSourceStop(sourceID);
+		alSourceUnqueueBuffers(sourceID, buffers);
+		renderedSeconds += (secondsPerBuffer * bufferCount);
+		if (position <= renderedSeconds) {
+			reset();
+			renderedSeconds = 0;
+		}
+		while (renderedSeconds < (position - secondsPerBuffer)) {
+			if (read(tempBytes) <= 0) break;
+			renderedSeconds += secondsPerBuffer;
+		}
+		boolean filled = false;
+		for (int i = 0; i < bufferCount; i++) {
+			int bufferID = buffers.get(i);
+			if (!fill(bufferID)) break;
+			filled = true;
+			alSourceQueueBuffers(sourceID, bufferID);
+		}
+		if (!filled) {
+			stop();
+			if (onCompletionListener != null) onCompletionListener.onCompletion(this);
+		}
+		alSourcef(sourceID, AL11.AL_SEC_OFFSET, position - renderedSeconds);
+		if (wasPlaying) {
+			alSourcePlay(sourceID);
+			isPlaying = true;
+		}
+	}
+	
+	public float getPosition () {
+		if (audio.noDevice) return 0;
+		if (sourceID == -1) return 0;
+		return renderedSeconds + alGetSourcef(sourceID, AL11.AL_SEC_OFFSET);
+	}
+	
+	public int read (byte[] buffer) {
+		if (input == null) {
+			input = new OggInputStream(file.read(), previousInput);
+			setup(input.getChannels(), input.getSampleRate());
+			previousInput = null; // release this reference
+		}
+		
+		return input.read(buffer);
+	}
+	
+	public void reset () {
+		StreamUtils.closeQuietly(input);
+		previousInput = null;
+		input = null;
+	}
+	
+	
+	protected void loop () {
+		StreamUtils.closeQuietly(input);
+		previousInput = input;
+		input = null;
+	}
+	
+	public int getChannels () {
+		return format == AL_FORMAT_STEREO16 ? 2 : 1;
+	}
+	
+	public int getRate () {
+		return sampleRate;
+	}
+	
+	public void update () {
+		if (audio.noDevice) return;
+		if (sourceID == -1) return;
+		
+		boolean end = false;
+		int buffers = alGetSourcei(sourceID, AL_BUFFERS_PROCESSED);
+		while (buffers-- > 0) {
+			int bufferID = alSourceUnqueueBuffers(sourceID);
+			if (bufferID == AL_INVALID_VALUE) break;
+			renderedSeconds += secondsPerBuffer;
+			if (end) continue;
+			if (fill(bufferID))
+				alSourceQueueBuffers(sourceID, bufferID);
+			else
+				end = true;
+		}
+		if (end && alGetSourcei(sourceID, AL_BUFFERS_QUEUED) == 0) {
+			stop();
+			if (onCompletionListener != null) onCompletionListener.onCompletion(this);
+		}
+		
+		// A buffer underflow will cause the source to stop.
+		if (isPlaying && alGetSourcei(sourceID, AL_SOURCE_STATE) != AL_PLAYING) alSourcePlay(sourceID);
+	}
+	
+	private boolean fill (int bufferID) {
+		tempBuffer.clear();
+		int length = read(tempBytes);
+		if (length <= 0) {
+			if (isLooping) {
+				loop();
+				renderedSeconds = 0;
+				length = read(tempBytes);
+				if (length <= 0) return false;
+			} else
+				return false;
+		}
+		tempBuffer.put(tempBytes, 0, length).flip();
+		alBufferData(bufferID, format, tempBuffer, sampleRate);
+		return true;
+	}
+	
+	public void dispose () {
+		stop();
+		if (audio.noDevice) return;
+		if (buffers == null) return;
+		//audio.freeSource(sourceID);
+		//sourceID=-1;
+		//alDeleteBuffers(buffers);
+		//buffers = null;
+		onCompletionListener = null;
+	}
+	
+	public void setOnCompletionListener (OnCompletionListener listener) {
+		onCompletionListener = listener;
+	}
+	
+	public int getSourceId () {
+		return sourceID;
+	}
+}
+
--- desktop/src/xyz/hexagons/client/desktop/DesktopAudioPlayer.java
+++ desktop/src/xyz/hexagons/client/desktop/DesktopAudioPlayer.java
@@ -1,248 +1,158 @@
 package xyz.hexagons.client.desktop;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.backends.lwjgl.audio.Ogg;
-import com.badlogic.gdx.backends.lwjgl.audio.OggInputStream;
-import com.badlogic.gdx.backends.lwjgl.audio.OpenALMusic;
+import com.badlogic.gdx.backends.lwjgl.audio.OpenALPlayer;
 import com.badlogic.gdx.files.FileHandle;
 import me.wieku.audio.analysis.BeatDetect;
 import me.wieku.audio.analysis.FFT;
-import org.lwjgl.BufferUtils;
+import xyz.hexagons.client.Instance;
 import xyz.hexagons.client.audio.AudioPlayer;
 import xyz.hexagons.client.utils.Utils;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
-public class DesktopAudioPlayer implements AudioPlayer{
-
-	private static void setFinalStatic(Field field, Object newValue) throws Exception {
-		field.setAccessible(true);
-
-		Field modifiersField = Field.class.getDeclaredField("modifiers");
-		modifiersField.setAccessible(true);
-		modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-
-		field.set(null, newValue);
-	}
-
-	//private class MethVisitor
-
-
+public class DesktopAudioPlayer implements AudioPlayer {
+	
+	public static OpenALPlayer musicPlayer;
+	
 	private boolean ended;
-	private boolean pause;
 	private FileHandle handle;
 	private boolean firstStart = false;
 	private FFT fft;
 	private BeatDetect detect;
 	private float[] outBuf = new float[60];
-	private OpenALMusic musicPlayer;
-
-	private static Field buffr;
-
+	
 	private static final int SIZE = 4096;
-	private byte[] bufferByte = new byte[SIZE*2];
-	private float[] bufferFloat = new float[SIZE*2];
+	private float[] bufferFloat = new float[SIZE * 2];
 	private float[] bufferFloat2 = new float[SIZE];
-
-	private float secPerBuffer;
-	private float audioLength;
-
-	private float bgvol, dstvol, timedelta = -1, time = -2;
-
-
+	
+	private float beginVolume, targetVolume, deltaTime = -1, time = -2;
+	
+	
 	public DesktopAudioPlayer(FileHandle fileHandle) {
-		if(!fileHandle.exists()) throw new IllegalStateException("Cannot find audio");
+		if (!fileHandle.exists()) throw new IllegalStateException("Cannot find audio");
 		handle = fileHandle;
-
-		musicPlayer = (OpenALMusic) Gdx.audio.newMusic(handle);
-		setSPB();
-		try {
-
-			Field inp = Ogg.Music.class.getDeclaredField("input");
-			inp.setAccessible(true);
-			OggInputStream str = (OggInputStream) inp.get(musicPlayer);
-
-			audioLength = ((str.getLength()*1f)/SIZE*2)/secPerBuffer;
-			System.out.println(audioLength);
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-
+		
+		if (musicPlayer == null) musicPlayer = new OpenALPlayer();
+		
+		musicPlayer.setFile(fileHandle);
 		musicPlayer.setOnCompletionListener(m -> ended = true);
-
+		
 		fft = new FFT(SIZE, musicPlayer.getRate());
 		fft.linAverages(60);
-
-		detect = new BeatDetect(SIZE*2, musicPlayer.getRate());
+		
+		detect = new BeatDetect(SIZE * 2, musicPlayer.getRate());
 		detect.setSensitivity(10);
 	}
-
-	private void setSPB() {
-		try {
-			Field sPB = OpenALMusic.class.getDeclaredField("secondsPerBuffer");
-			sPB.setAccessible(true);
-			sPB.set(musicPlayer, (float)SIZE*2/(musicPlayer.getChannels()*musicPlayer.getRate()*2));
-			secPerBuffer = (float) sPB.get(musicPlayer);
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-	}
-
+	
 	@Override
 	public FileHandle getFile() {
 		return handle;
 	}
-
+	
 	@Override
 	public float getVolume() {
 		return musicPlayer.getVolume();
 	}
-
+	
 	@Override
 	public void setVolume(float volume) {
 		musicPlayer.setVolume(volume);
 	}
-
+	
 	@Override
 	public float getPosition() {
 		return musicPlayer.getPosition();
 	}
-
+	
 	@Override
 	public void setPosition(float milis) {
-		System.out.println("llllll "+milis);
-		//Gdx.app.postRunnable(()->musicPlayer.setPosition(milis));
-		new Thread(()->{
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			float vol = musicPlayer.getVolume();
-			setVolume(0);
-			musicPlayer.setPosition(milis);
-			if(pause) musicPlayer.pause();
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			setVolume(vol);
-			System.out.println(musicPlayer.getPosition());
-		}).start();
+		Instance.executor.execute(() -> musicPlayer.setPosition(milis));
 	}
-
+	
 	@Override
-	public void play(){
+	public void play() {
 		musicPlayer.play();
 		firstStart = true;
 		ended = false;
-		pause = false;
 	}
-
+	
 	@Override
-	public void play(float time){
+	public void play(float time) {
 		musicPlayer.play();
-		if(firstStart || time > 0){
+		if (firstStart || time > 0) {
 			setPosition(time);
 		}
 		firstStart = true;
 		ended = false;
-
+		
 	}
-
+	
 	@Override
-	public void update(float delta){
-
-
-		if(timedelta < time){
-			timedelta+=delta;
-			setVolume(bgvol+((dstvol-bgvol)*timedelta)/time);
+	public void update(float delta) {
+		
+		if (deltaTime < time) {
+			deltaTime += delta;
+			setVolume(beginVolume + ((targetVolume - beginVolume) * deltaTime) / time);
 		}
-
-
-		try {
-			bufferByte = (byte[]) buffr.get(null);
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-
-		Utils.byteToFloat(bufferByte, 0, bufferFloat, 0, bufferFloat.length);
-
-		for(int i = 0, j = 0; i < bufferFloat2.length; i++, j+=2)
-			bufferFloat2[i] = (bufferFloat[j] + bufferFloat[j+1]) / 2;
-
+		
+		Utils.byteToFloat(musicPlayer.tempBytes, 0, bufferFloat, 0, bufferFloat.length);
+		
+		for (int i = 0, j = 0; i < bufferFloat2.length; i++, j += 2)
+			bufferFloat2[i] = (bufferFloat[j] + bufferFloat[j + 1]) / 2;
+		
 		fft.forward(bufferFloat2);
-
-		for(int i = 0; i < fft.avgSize(); i++)
+		
+		for (int i = 0; i < fft.avgSize(); i++)
 			outBuf[i] = fft.getAvg(i);
-
+		
 		detect.detect(bufferFloat);
 	}
-
+	
 	@Override
-	public void glideVolume(float volume, float time){
-		bgvol = getVolume();
-		dstvol = volume;
+	public void glideVolume(float volume, float time) {
+		beginVolume = getVolume();
+		targetVolume = volume;
 		this.time = time;
-		timedelta=0;
+		deltaTime = 0;
 	}
-
+	
 	@Override
-	public float[] getFFT(){
+	public float[] getFFT() {
 		return outBuf;
 	}
-
+	
 	@Override
 	public boolean isOnset() {
-		return detect.isRange(1,3,2);// || detect.isHat()/*Range(1,3,2) || detect.isRange(4,6,2)*/;
+		return detect.isRange(1, 3, 2);
 	}
-
+	
 	@Override
-	public void pause(){
-		pause = true;
+	public void pause() {
 		musicPlayer.pause();
 	}
-
+	
 	@Override
-	public boolean hasEnded(){
+	public boolean hasEnded() {
 		return ended;
 	}
-
+	
 	@Override
-	public void stop(){
+	public void stop() {
 		musicPlayer.stop();
 		ended = true;
 	}
-
+	
 	@Override
-	public void dispose(){
+	public void dispose() {
 		musicPlayer.dispose();
 	}
-
+	
 	@Override
 	public boolean isPaused() {
 		return !musicPlayer.isPlaying() && !ended;
 	}
-
+	
 	@Override
 	public void setLooping(boolean looping) {
 		musicPlayer.setLooping(looping);
 	}
-
-	static {
-		try {
-
-			setFinalStatic(OpenALMusic.class.getDeclaredField("bufferSize"), SIZE*2);
-			setFinalStatic(OpenALMusic.class.getDeclaredField("tempBytes"), new byte[SIZE*2]);
-			setFinalStatic(OpenALMusic.class.getDeclaredField("tempBuffer"), BufferUtils.createByteBuffer(SIZE*2));
-
-			buffr = OpenALMusic.class.getDeclaredField("tempBytes");
-			buffr.setAccessible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
+	
 }
