39fdb6986f04feab013b09d2abfe4b864f457459
Łukasz Magiera <magik6k@gmail.com>
2016-12-25 17:58:05 +0100

    Prepare bit of sql

--- rankserv/resources/sql/prepare.sql
+++ rankserv/resources/sql/prepare.sql
@@ -17,3 +17,28 @@ CREATE TABLE IF NOT EXISTS user_auth( user_id INTEGER,
                                       type INTEGER,
                                       data VARCHAR(64),
                                       FOREIGN KEY(user_id) REFERENCES users(id));
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS top_scores AS
+                                      SELECT nick, map_id, MAX(score) AS sc FROM games GROUP BY map_id, nick;
+
+CREATE UNIQUE INDEX IF NOT EXISTS top_scores_key on top_scores (nick, map_id);
+
+CREATE OR REPLACE FUNCTION update_scores()
+  RETURNS trigger AS
+  $$
+    BEGIN
+      REFRESH MATERIALIZED VIEW CONCURRENTLY top_scores;
+      RETURN null;
+    END;
+  $$
+  LANGUAGE 'plpgsql';
+
+
+
+
+DROP TRIGGER IF EXISTS test_trigger ON games;
+CREATE TRIGGER test_trigger
+  AFTER INSERT
+    ON games
+  FOR EACH STATEMENT
+  EXECUTE PROCEDURE update_scores();
